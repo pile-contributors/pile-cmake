c3214d7c3b5225822ad0fc0c19b22763fad5dc32
Nicu Tofan <nicu.tofan@gmail.com>
2015-03-06 18:32:39 +0200

    Implemented git querries at configure time for commit and branch

--- pile_git.cmake
+++ pile_git.cmake
@@ -14,6 +14,7 @@
 #    include(pile_support)
 #
 # ============================================================================
+find_package(Git)
 
 # ============================================================================
 
@@ -31,7 +32,37 @@ macro    (pileGitStamp
           pile_git_stamp__commit
           pile_git_stamp__branch)
 
-    # TODO
+    set(pile_git_stamp__describe_build )
+    set(pile_git_stamp__err )
+    if(GIT_FOUND)
+
+        execute_process(
+                COMMAND ${GIT_EXECUTABLE} log -1 --pretty=%H
+                WORKING_DIRECTORY ${pile_git_stamp__path}
+                OUTPUT_VARIABLE   ${pile_git_stamp__commit}
+                ERROR_VARIABLE pile_git_stamp__err
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+        if (pile_git_stamp__err)
+            message(WARNING "Error extracting git commit: ${pile_git_stamp__err}")
+            set(${pile_git_stamp__commit} "error")
+        endif()
+
+        execute_process(
+                COMMAND ${GIT_EXECUTABLE} symbolic-ref --short HEAD
+                WORKING_DIRECTORY ${pile_git_stamp__path}
+                OUTPUT_VARIABLE   ${pile_git_stamp__branch}
+                ERROR_VARIABLE pile_git_stamp__err
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+        if (pile_git_stamp__err)
+            message(WARNING "Error extracting git branch: ${pile_git_stamp__err}")
+            set(${pile_git_stamp__branch} "error")
+        endif()
+
+    else()
+        set(${pile_git_stamp__commit} "git not found")
+        set(${pile_git_stamp__branch} "git not found")
+    endif()
+
 endmacro ()
 
 # ============================================================================
@@ -45,9 +76,31 @@ endmacro ()
 #     - footer (optional): text to add after the definition
 #
 macro    (pileGitStampHeader
-          pile_git_stamp_header__path)
+          pile_git_stamp_header__path
+          pile_git_stamp_header__out
+          pile_git_stamp_header__header
+          pile_git_stamp_header__footer)
 
-    # TODO
+    # get the info from git
+    set (pile_git_stamp_header__branch )
+    set (pile_git_stamp_header__commit )
+    pileGitStamp(${pile_git_stamp_header__path}
+        pile_git_stamp_header__branch
+        pile_git_stamp_header__commit)
+
+    file(WRITE "${pile_git_stamp_header__out}"
+"${pile_git_stamp_header__header}
+
+#ifndef SOURCE_CODE_GIT_HASH
+#   define SOURCE_CODE_GIT_HASH ${pile_git_stamp_header__commit}
+#endif // SOURCE_CODE_GIT_HASH
+
+#ifndef SOURCE_CODE_GIT_BRANCH
+#   define SOURCE_CODE_GIT_BRANCH ${pile_git_stamp_header__branch}
+#endif // SOURCE_CODE_GIT_BRANCH
+
+${pile_git_stamp_header__footer}
+")
 
 endmacro ()
 
@@ -64,9 +117,39 @@ endmacro ()
 # If the file already exists, its content is inspected to see if an update is
 # required.
 macro    (pileGitStampHeaderCheck
-          pile_git_stamp_header_check__path)
+          pile_git_stamp_header_check__path
+          pile_git_stamp_header_check__out
+          pile_git_stamp_header_check__header
+          pile_git_stamp_header_check__footer)
 
-    # TODO
+    set (pile_git_stamp_header_check__branch )
+    set (pile_git_stamp_header_check__commit )
+    pileGitStamp(${pile_git_stamp_header_check__path}
+        pile_git_stamp_header_check__branch
+        pile_git_stamp_header_check__commit)
+
+    set (pile_git_stamp_header_check__intermed
+        "${CMAKE_CURRENT_BINARY_DIR}/git_info.h.in")
+    if(NOT EXISTS ${pile_git_stamp_header_check__intermed})
+        file(WRITE "${pile_git_stamp_header_check__intermed}"
+"${pile_git_stamp_header__header}
+
+#ifndef SOURCE_CODE_GIT_HASH
+#   define SOURCE_CODE_GIT_HASH @SOURCE_CODE_GIT_HASH@
+#endif // SOURCE_CODE_GIT_HASH
+
+#ifndef SOURCE_CODE_GIT_BRANCH
+#   define SOURCE_CODE_GIT_BRANCH @SOURCE_CODE_GIT_BRANCH@
+#endif // SOURCE_CODE_GIT_BRANCH
+
+${pile_git_stamp_header__footer}
+")
+    endif()
+
+    configure_file(
+        "${pile_git_stamp_header_check__intermed}"
+        "${pile_git_stamp_header_check__out}"
+       @ONLY)
 
 endmacro ()
 

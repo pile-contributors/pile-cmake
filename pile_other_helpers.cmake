695eafa13d0409cb5bc3f5cfca58212776e03ea9
Nicu Tofan <nicu.tofan@gmail.com>
2015-12-02 21:13:40 +0200

    Bug in naming the variable PROJECT_NAME_UPPER

--- pile_other_helpers.cmake
+++ pile_other_helpers.cmake
@@ -2,7 +2,7 @@
 # ============================================================================
 
 # Set the name
-# 
+#
 # Sets the name to either the simple form or with a version.
 #
 # LOC_PRJ_NAME is set to the final form of the name.
@@ -13,7 +13,7 @@ macro    (pileSetLocalName pile_local_name)
         SET ( LOC_PRJ_NAME "${pile_local_name}" )
         SET ( ${pile_local_name}  "${LOC_PRJ_NAME}" CACHE INTERNAL "${pile_local_name}")
     ELSE(WIN32)
-        SET ( LOC_PRJ_NAME "${pile_local_name}-${${PROJECT_NAME_U}_VERSION}" )
+        SET ( LOC_PRJ_NAME "${pile_local_name}-${${PROJECT_NAME_UPPER}_VERSION}" )
         SET ( ${pile_local_name}  "${LOC_PRJ_NAME}" CACHE INTERNAL "${pile_local_name}")
     ENDIF(WIN32)
 endmacro ()

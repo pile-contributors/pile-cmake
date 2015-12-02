4e1a33c58e899a0e6d654c8b8ef5d3a3bb311c1b
Nicu Tofan <nicu.tofan@gmail.com>
2015-12-02 21:12:45 +0200

    bug in commit/branch; added prefix for output

--- pile_git.cmake
+++ pile_git.cmake
@@ -35,27 +35,37 @@ macro    (pileGitStamp
     set(pile_git_stamp__describe_build )
     set(pile_git_stamp__err )
     if(GIT_FOUND)
-
+        unset(pile_git_stamp__err)
+        unset(pile_git_stamp__temp)
         execute_process(
                 COMMAND ${GIT_EXECUTABLE} log -1 --pretty=%H
                 WORKING_DIRECTORY ${pile_git_stamp__path}
-                OUTPUT_VARIABLE   ${pile_git_stamp__commit}
-                ERROR_VARIABLE pile_git_stamp__err
+                OUTPUT_VARIABLE   pile_git_stamp__temp
+                ERROR_VARIABLE    pile_git_stamp__err
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
         if (pile_git_stamp__err)
             message(WARNING "Error extracting git commit from ${pile_git_stamp__path}: ${pile_git_stamp__err}")
             set(${pile_git_stamp__commit} "error")
+        else()
+            STRING(REGEX REPLACE "\n" "" pile_git_stamp__temp "${pile_git_stamp__temp}")
+            STRING(REGEX REPLACE "\r" "" pile_git_stamp__temp "${pile_git_stamp__temp}")
+            string(STRIP "${pile_git_stamp__temp}" ${pile_git_stamp__commit})
         endif()
 
+        unset(pile_git_stamp__temp)
         execute_process(
                 COMMAND ${GIT_EXECUTABLE} symbolic-ref --short HEAD
                 WORKING_DIRECTORY ${pile_git_stamp__path}
-                OUTPUT_VARIABLE   ${pile_git_stamp__branch}
-                ERROR_VARIABLE pile_git_stamp__err
+                OUTPUT_VARIABLE   pile_git_stamp__temp
+                ERROR_VARIABLE    pile_git_stamp__err
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
         if (pile_git_stamp__err)
             message(WARNING "Error extracting git branch: ${pile_git_stamp__err}")
             set(${pile_git_stamp__branch} "error")
+        else()
+            STRING(REGEX REPLACE "\n" "" pile_git_stamp__temp "${pile_git_stamp__temp}")
+            STRING(REGEX REPLACE "\r" "" pile_git_stamp__temp "${pile_git_stamp__temp}")
+            string(STRIP "${pile_git_stamp__temp}" ${pile_git_stamp__branch})
         endif()
 
     else()
@@ -82,17 +92,17 @@ macro    (pileGitStampHeader
           pile_git_stamp_header__footer)
 
     # get the info from git
-    set (pile_git_stamp_header__branch )
-    set (pile_git_stamp_header__commit )
+    unset (pile_git_stamp_header__branch )
+    unset (pile_git_stamp_header__commit )
     pileGitStamp(${pile_git_stamp_header__path}
-        pile_git_stamp_header__branch
-        pile_git_stamp_header__commit)
+        pile_git_stamp_header__commit
+        pile_git_stamp_header__branch)
 
     file(WRITE "${pile_git_stamp_header__out}"
 "${pile_git_stamp_header__header}
 
 #ifndef SOURCE_CODE_GIT_HASH
-#   define SOURCE_CODE_GIT_HASH ${pile_git_stamp_header__commit}
+#   define SOURCE_CODE_GIT_HASH \"${pile_git_stamp_header__commit}\"
 #endif // SOURCE_CODE_GIT_HASH
 
 #ifndef SOURCE_CODE_GIT_BRANCH
@@ -120,13 +130,15 @@ macro    (pileGitStampHeaderCheck
           pile_git_stamp_header_check__path
           pile_git_stamp_header_check__out
           pile_git_stamp_header_check__header
-          pile_git_stamp_header_check__footer)
+          pile_git_stamp_header_check__footer
+          pile_git_stamp_header_check__prefix)
 
-    set (pile_git_stamp_header_check__branch )
-    set (pile_git_stamp_header_check__commit )
+    unset (pile_git_stamp_header_check__branch )
+    unset (pile_git_stamp_header_check__commit )
     pileGitStamp(${pile_git_stamp_header_check__path}
-        pile_git_stamp_header_check__branch
-        pile_git_stamp_header_check__commit)
+        pile_git_stamp_header_check__commit
+        pile_git_stamp_header_check__branch)
+
 
     set (pile_git_stamp_header_check__intermed
         "${CMAKE_CURRENT_BINARY_DIR}/git_info.h.in")
@@ -134,17 +146,25 @@ macro    (pileGitStampHeaderCheck
         file(WRITE "${pile_git_stamp_header_check__intermed}"
 "${pile_git_stamp_header_check__header}
 
-#ifndef SOURCE_CODE_GIT_HASH
-#   define SOURCE_CODE_GIT_HASH @SOURCE_CODE_GIT_HASH@
-#endif // SOURCE_CODE_GIT_HASH
+#ifndef ${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_HASH
+#   define ${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_HASH \"@${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_HASH@\"
+#endif // ${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_HASH
 
-#ifndef SOURCE_CODE_GIT_BRANCH
-#   define SOURCE_CODE_GIT_BRANCH @SOURCE_CODE_GIT_BRANCH@
-#endif // SOURCE_CODE_GIT_BRANCH
+#ifndef ${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_BRANCH
+#   define ${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_BRANCH @${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_BRANCH@
+#endif // ${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_BRANCH
 
 ${pile_git_stamp_header_check__footer}
 ")
     endif()
+
+    set (${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_HASH "${pile_git_stamp_header_check__commit}"
+         CACHE STRING "Git hash of source code for ${pile_git_stamp_header_check__prefix}" FORCE)
+    set (${pile_git_stamp_header_check__prefix}_SOURCE_CODE_GIT_BRANCH "${pile_git_stamp_header_check__branch}"
+         CACHE STRING "Git branch of source code for ${pile_git_stamp_header_check__prefix}" FORCE)
+
+    unset(pile_git_stamp_header_check__commit)
+    unset(pile_git_stamp_header_check__branch)
 
     configure_file(
         "${pile_git_stamp_header_check__intermed}"
